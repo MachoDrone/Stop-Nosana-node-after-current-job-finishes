de003c1653c8aba77769f6b8586c49e9a05d7030
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-29 23:15:31 -0400

    Create finishjob-installer.sh

---
+++ finishjob-installer.sh
@@ -0,0 +1,6 @@
+rm -r -f finishjob.sh
+wget https://raw.githubusercontent.com/MachoDrone/Stop-Nosana-node-after-current-job-finishes/main/finishjob.sh
+sudo chmod +x finishjob.sh
+echo "******************************************************"
+echo "   Run the scipt at any time with: ./finishjob.sh"
+echo "******************************************************"
